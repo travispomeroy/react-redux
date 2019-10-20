342e19a57562caa9de52047de3d24736c7d73cc9
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-10-20 11:45:06 -0400

    Removed a lot of logic out of the CoursesPage component since it is just a container component.

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -12,57 +12,21 @@ export interface Course {
     category: string;
 }
 
-export interface Author {
-    id: bigint;
-    name: string;
-}
-
 export interface CoursesPageState {
     courses: Course[];
     actions: typeof courseActions;
 }
 
-class CoursesPage extends React.Component<CoursesPageState, {course: Course;}> {
-    constructor(props: Readonly<CoursesPageState>) {
-        super(props);
-        this.state = {
-            course: {
-                id: "",
-                title: "",
-                slug: "",
-                authorId: 0,
-                category: ""
-            }
-        };
-    }
-
-    private handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        this.props.actions.createCourse(this.state.course);
-    };
-
-    private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const course: Course = {
-            ...this.state.course,
-            title: event.target.value
-        };
-
-        this.setState({course});
-    };
+class CoursesPage extends React.Component<CoursesPageState> {
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add Course</h3>
-                <input type="text" onChange={this.handleChange} value={this.state.course.title}/>
-                <input type="submit" value="Save"/>
                 {
-                    this.props.courses.map(x => (
-                        <div key={x.title}>{x.title}</div>
-                    ))
+                    this.props.courses.map(x => <div key={x.title}>{x.title}</div>)
                 }
-            </form>
+            </>
         );
     }
 }
